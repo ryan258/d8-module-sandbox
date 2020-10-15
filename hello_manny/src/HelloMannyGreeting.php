c0607baedc736ead460befba91cf7e0dd1190309
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-10-15 10:48:26 -0500

    Delegate the greeting logic to a service.

---
+++ hello_manny/src/HelloMannyGreeting.php
@@ -0,0 +1,28 @@
+<?php
+namespace Drupal\hello_manny;
+
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+
+/**
+ * Prepares a greeting for the Manny.
+ */
+
+class HelloMannyGreeting {
+  use StringTranslationTrait;
+
+  /**
+   * Returns the greeting 
+   */
+  public function getGreeting() {
+    $time = new \DateTime();
+    if ((int) $time->format('G') >= 00 && (int) $time->format('G') < 12) {
+      return $this->t('Good morning Manny');
+    }
+    if ((int) $time->format('G') >= 12 && (int) $time->format('G') < 18) {
+      return $this->t('Good evening Manny');
+    }
+    if ((int) $time->format('G') >= 18) {
+      return $this->t('Good evening Manny');
+    }
+  }
+}
